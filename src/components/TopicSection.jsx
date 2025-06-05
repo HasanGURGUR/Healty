9457f858f0322c94ef74c6bd2cb9385b9cb65052
HasanGURGUR <hasangurgur95@gmail.com>
2025-06-05 22:05:15 +0300

    items ui fixed

--- src/components/TopicSection.jsx
+++ src/components/TopicSection.jsx
@@ -7,18 +7,20 @@ const TopicSection = ({ items }) => {
     <section className="topic-section">
       <div className="topic-items">
         {items.map((item, index) => (
-          <div key={index} className="topic-item">
-            <div className="topic-image-container">
-              <img src={item.image} alt={item.title} className="topic-image" />
+          <Link to={item.link} key={index} className="topic-item-link">
+            <div className="topic-item">
+              <div className="topic-image-container">
+                <img src={item.image} alt={item.title} className="topic-image" />
+              </div>
+              <div className="topic-content">
+                <h3 className="topic-title">{item.title}</h3>
+                <p className="topic-description">{item.description}</p>
+                <div className="go-to-topic-button">
+                  GO TO TOPIC &rarr;
+                </div>
+              </div>
             </div>
-            <div className="topic-content">
-              <h3 className="topic-title">{item.title}</h3>
-              <p className="topic-description">{item.description}</p>
-              <Link to={item.link} className="go-to-topic-button">
-                GO TO TOPIC &rarr;
-              </Link>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
