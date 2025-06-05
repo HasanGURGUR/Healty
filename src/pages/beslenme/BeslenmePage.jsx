a599413e2b97cbc327a4e316254795f74da65db0
HasanGURGUR <hasangurgur95@gmail.com>
2025-06-06 01:02:52 +0300

    beslenme sayfası banner eklendi

--- src/pages/beslenme/BeslenmePage.jsx
+++ src/pages/beslenme/BeslenmePage.jsx
@@ -3,11 +3,11 @@ import NutritionBanner from '../../components/NutritionBanner';
 
 const BeslenmePage = () => {
   return (
-    <div>
+    <>
       <NutritionBanner />
       <h1>Beslenme Sayfası</h1>
       {/* Bu sayfaya özgü içerik buraya gelecek */}
-    </div>
+    </>
   );
 };
 
